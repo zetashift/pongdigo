c48c3073c19f0dfbb14227ca2d7fbd96bbd997d7
zetashift <rskaraya@gmail.com>
2023-04-17 18:25:03 +0200

    Indigo should grab the right element now

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 import './style.css'
 import * as indigo from 'scalajs:main.js'
 
-indigo.IndigoGame.launch({
-  "width": window.innerWidth.toString(),
-  "height": window.innerHeight.toString()
-})
-
-
+indigo.IndigoGame.launch("indigo-container")
